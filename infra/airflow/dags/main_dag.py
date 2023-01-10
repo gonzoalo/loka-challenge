87e2b9dd2b43ad172576180abd7ac97800e7891c
3voor <gonzalo.sanchezc@pucp.edu.pe>
2023-01-10 15:48:08 -0500

    fetch data finished

--- infra/airflow/dags/main_dag.py
+++ infra/airflow/dags/main_dag.py
@@ -1,31 +1,25 @@
 from airflow import DAG
-
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from airflow.providers.amazon.aws.operators.s3 import S3CopyObjectOperator
+from airflow.hooks.S3_hook import S3Hook
 from airflow.utils.dates import days_ago
 from airflow.models.baseoperator import chain
 
-import boto3
-
-
+import botocore
+import re
 from datetime import datetime, timedelta
 
-MAIN_SOURCE_BUCKET = "s3://de-tech-assessment-2022/"
+# send this to .env file
+MAIN_SOURCE_BUCKET = "de-tech-assessment-2022"
 DATA_FOLDER = "data/"
-DATALAKE_BUCKET = "s3://de-tech-assessment-2022-gonzalo"
+DATALAKE_BUCKET = "de-tech-assessment-2022-gonzalo"
 RAW_ZONE_FOLDER = "raw_zone/"
 STRUCTURE_ZONE_FOLDER = "structure_zone/"
-
+AWS_DEFAULT_CONN = "aws_default"
 
 default_args = {
     'retry': 5,
     'retry_delay': timedelta(minutes=5)
 }
-
-def _extract_data(ti, **kwargs):
-    s3_link_location = "s3://de-tech-assessment-2022/data/"
-    
 
 def fetch_data(ti, **kwargs):
     """
@@ -38,12 +32,39 @@ def fetch_data(ti, **kwargs):
     - ti:
     - kwargs:
     """
+
     datetime_format = "%Y-%m-%d %H-%M-%S"
     date_tag = datetime.now().strftime(datetime_format)
     test_tag = "2019-06-01-15-29-5"
 
-    session = boto3.Session()
+    s3_hook = S3Hook(aws_conn_id=AWS_DEFAULT_CONN)
+    s3_hook.get_conn()
 
+    prefix = f"{DATA_FOLDER}{test_tag}"
+    data_objects = s3_hook.list_keys(bucket_name=MAIN_SOURCE_BUCKET, prefix=prefix)
+
+    for data_object in data_objects:
+        object_key = re.sub(DATA_FOLDER, '', data_object)
+        if re.findall('.json', object_key):
+            source_bucket_key = f"s3://{MAIN_SOURCE_BUCKET}/{DATA_FOLDER}{object_key}"
+            dest_bucket_key = f"s3://{DATALAKE_BUCKET}/{RAW_ZONE_FOLDER}{object_key}"
+
+            try:
+                response = s3_hook.copy_object( 
+                    source_bucket_key=source_bucket_key,
+                    dest_bucket_key=dest_bucket_key
+                )
+
+                if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                    pass
+                else:
+                    print(f"The file {object_key} was not properly copied.")
+                
+            except botocore.exceptions.ClientError as error:
+                raise error
+            
+            except botocore.exceptions.ParamValidationError as error:
+                raise ValueError('The parameters you provided are incorrect: {}'.format(error))
 
 
 
